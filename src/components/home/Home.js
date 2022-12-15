739d547e6fe9e2003c7d062325136687990ff2bf
FNogueira2023 <f.nogueira2022@outlook.com>
2022-12-15 17:44:44 -0300

    Iniciando el navbar

---
+++ src/components/home/Home.js
@@ -0,0 +1,17 @@
+import NavBar from "./NavBar";
+import '../../public/stylesheets/home.css';
+
+function Home() {
+    return (
+        <>        
+        <NavBar />
+
+        <div className="container-home">    
+        
+      
+        </div>
+        </>
+    )
+}
+
+export default Home;
