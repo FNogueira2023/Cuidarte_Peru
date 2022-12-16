b5d1577292ce0c83fcd6003ef0b26d9c97c2feac
FNogueira2023 <f.nogueira2022@outlook.com>
2022-12-16 12:49:12 -0300

    Terminado navbar desktop

--- src/components/home/NavBar.js
+++ src/components/home/NavBar.js
@@ -2,28 +2,58 @@ import '../../public/stylesheets/navbar.css';
 
 function NavBar() {
     return (
-        <navbar className='navbar'>
+        <nav className='topbar'>
             <div className='logo'>
-                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-prescription2" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" className="bi bi-prescription2 topbar-icon" viewBox="0 0 16 16">
                     <path d="M7 6h2v2h2v2H9v2H7v-2H5V8h2V6Z" />
                     <path d="M2 1a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1v10.5a1.5 1.5 0 0 1-1.5 1.5h-7A1.5 1.5 0 0 1 3 14.5V4a1 1 0 0 1-1-1V1Zm2 3v10.5a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 .5-.5V4H4ZM3 3h10V1H3v2Z" />
                 </svg>
-                <h3>Cuidarte Perú</h3>
+                <div className='topbar-title'>
+                    <h4>Boticas</h4>
+                    <h3>Cuidarte Perú</h3>
+                </div>
+
             </div>
 
 
-            <div class="search">
-                <input type="text" class="searchTerm" placeholder="What are you looking for?" />
-                <button type="submit" class="searchButton">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+            <div className="search">
+                <input type="text" className="searchTerm" placeholder="Qué estás buscando?" />
+                <button type="submit" className="searchButton">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
                         <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
                     </svg>
                 </button>
             </div>
 
+            <div className="user-icons">
+
+                <div className="login-icon">
+                    <a to="#">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-person" viewBox="0 0 16 16">
+                            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z" />
+                        </svg>
+                        <span>Ingresar</span>
+                    </a>
+                </div>
+
+                <div className="card-icon">
+                    <a to="#">
+                        <div className='cart-icon'>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-cart-plus" viewBox="0 0 16 16">
+                                <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9V5.5z" />
+                                <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
+                            </svg>
+                            <span>Carrito</span>
+                        </div>
+                    </a>
+
+                </div>
+            </div>
 
 
-        </navbar >
+
+
+        </nav >
     )
 }
 
